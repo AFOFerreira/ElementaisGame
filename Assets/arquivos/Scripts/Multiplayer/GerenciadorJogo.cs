efa89ec16553fc78d3a241b46de8140c1f6c80a8
Cod3rN3rd <desenvolvimento.abnfel@outlook.com>
2021-05-28 22:57:56 -0300

    Começo da integracao do gerenciador de jogo de teste, com o definitivo.

--- Assets/arquivos/Scripts/Multiplayer/GerenciadorJogo.cs
+++ Assets/arquivos/Scripts/Multiplayer/GerenciadorJogo.cs
@@ -50,6 +50,28 @@ public class GerenciadorJogo : MonoBehaviourPunCallbacks
 
     public List<Sprite> marcadoresAtaque;
 
+    public static GerenciadorJogo instance;
+    public List<CampoSingle> camposIa;
+    public List<CampoSingle> camposPlayer;
+
+    public TipoJogador turno;
+    public TipoFase faseAtual;
+    public int fase;
+
+    public bool EmJogo;
+    public bool turnoDefesa;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -80,6 +102,25 @@ public class GerenciadorJogo : MonoBehaviourPunCallbacks
         }
 
         tempoCronometro = 18;
+
+        EmJogo = true;
+        sorteio();
+        fase = 1;
+        //turno = TipoJogador.IA;
+        if (EmJogo)
+        {
+            foreach (var obj in Transform.FindObjectsOfType<CampoSingle>())
+            {
+                if (obj.donoCampo == TipoJogador.PLAYER)
+                {
+                    camposPlayer.Add(obj);
+                }
+                else
+                {
+                    camposIa.Add(obj);
+                }
+            }
+        }
     }
 
     private void OnEnable()
@@ -817,4 +858,145 @@ public class GerenciadorJogo : MonoBehaviourPunCallbacks
         }
 
     }
+
+
+    #region controle de campo
+    public List<CampoSingle> VerificaCampoDisponivelMagicas(TipoJogador tipoJogador)
+    {
+        var CamposVazios = new List<CampoSingle>();
+        if (tipoJogador == TipoJogador.IA)
+        {
+            foreach (var obj in camposIa)
+            {
+                if (obj.vazio == true && obj.tipoCartaCampo == TipoCarta.MAGICA)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            foreach (var obj in camposPlayer)
+            {
+                if (obj.vazio == true && obj.tipoCartaCampo == TipoCarta.MAGICA)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        return CamposVazios;
+    }
+    public List<CampoSingle> VerificaCampoOcupadoMagicas(TipoJogador tipoJogador)
+    {
+        var CamposVazios = new List<CampoSingle>();
+        if (tipoJogador == TipoJogador.IA)
+        {
+            foreach (var obj in camposIa)
+            {
+                if (obj.vazio == false && obj.tipoCartaCampo == TipoCarta.MAGICA)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            foreach (var obj in camposPlayer)
+            {
+                if (obj.vazio == false && obj.tipoCartaCampo == TipoCarta.MAGICA)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        return CamposVazios;
+    }
+    public List<CampoSingle> VerificaCampoDisponivelMonstros(TipoJogador tipoJogador)
+    {
+        var CamposVazios = new List<CampoSingle>();
+        if (tipoJogador == TipoJogador.IA)
+        {
+            foreach (var obj in camposIa)
+            {
+                if (obj.vazio == true && obj.tipoCartaCampo == TipoCarta.ELEMENTAL)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            foreach (var obj in camposPlayer)
+            {
+                if (obj.vazio == true && obj.tipoCartaCampo == TipoCarta.ELEMENTAL)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        return CamposVazios;
+    }
+    public List<CampoSingle> VerificaCampoOcupadoMonstros(TipoJogador tipoJogador)
+    {
+        var CamposVazios = new List<CampoSingle>();
+        if (tipoJogador == TipoJogador.IA)
+        {
+            foreach (var obj in camposIa)
+            {
+                if (obj.vazio == false && obj.tipoCartaCampo == TipoCarta.ELEMENTAL)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            foreach (var obj in camposPlayer)
+            {
+                if (obj.vazio == false && obj.tipoCartaCampo == TipoCarta.ELEMENTAL)
+                {
+                    CamposVazios.Add(obj);
+                }
+            }
+        }
+        return CamposVazios;
+    }
+    #endregion
+
+    #region controle turno
+    public void PassaTurno()
+    {
+        if (turno == TipoJogador.IA)
+        {
+            turno = TipoJogador.PLAYER;
+        }
+        else
+        {
+            turno = TipoJogador.IA;
+        }
+        faseAtual = TipoFase.MONSTRO;
+    }
+    #endregion
+
+    #region controle sorteio
+    public void sorteio()
+    {
+        int valor = Random.Range(0, 100);
+        if (valor >= 0 && valor <= 49)
+        {
+            turno = TipoJogador.PLAYER;
+        }
+        else
+        {
+            turno = TipoJogador.IA;
+        }
+    }
+    #endregion
+
+    #region Controle de jogo
+    public void JogarCarta(CampoSingle campo, GameObject carta)
+    {
+        Instantiate(carta, campo.containerCarta.transform);
+    }
+    #endregion
 }
